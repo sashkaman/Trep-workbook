60f89d673902bfe8df36a99907b08e4ef57cd43e
Alexander <rzn.russia4912@gmail.com>
2026-05-18 23:46:17 +0300

    check commit 18.05 	new file:   Tasks -1.js

---
+++ Tasks -1.js
@@ -0,0 +1,60 @@
+// Level 1.1 task JavaScript
+
+// #1
+
+// let num = -24234;
+// const checkProcess = (value) => {
+//     return value > 0 ? value > 0 : value < 0
+// }
+// checkProcess(num)
+
+// #2
+// let string = 'whats_up';
+// const checkProcess = (value) => {
+//     console.log(value.length)
+// }
+// checkProcess(string)
+
+// #3
+// let string = 'whats_up';
+// const checkProcess = (value) => {
+//     return value.length - 1)
+//     return value.at(-1))
+// }
+// console.log(checkProcess(string))
+
+// #4
+// let number = 25;
+
+// const checkProcess = (value) => {
+//         return value % 2 == 0;
+//     }
+//     console.log(checkProcess(number));
+
+// #5
+
+// let word1 = 'what_up man';
+// let word2 = 'what is your name?';
+
+// const checkProcess = (value1, va2lue2) => {
+//     return value1.at(0) == value.at(0);
+// }
+// console.log(checkProcess(word1, word2));
+
+// #6
+// let word1 = 'what_up man';
+// let word2 = 'what is your name?';
+
+// const checkProcess = (value1, va2lue2) => {
+//     return value1.at(0) == value.at(0);
+// }
+// console.log(checkProcess(word1, word2));
+
+// Level 1.2 task JavaScript
+// #1
+
+// let number = 25545;
+// const checkProcess = (value) => {
+//     return value.toString().at(0);
+// }
+// console.log(checkProcess(number));

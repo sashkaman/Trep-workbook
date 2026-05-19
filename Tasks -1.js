335ab289ddebd430591204216d5a88de3fb6b8ce
Alexander <rzn.russia4912@gmail.com>
2026-05-19 22:45:14 +0300

    added new level 1.2 	modified:   Tasks -1.js

--- Tasks -1.js
+++ Tasks -1.js
@@ -1,4 +1,4 @@
-// Level 1.1 task JavaScript
+Level 1.1 task JavaScript
 
 #1
 
@@ -8,28 +8,28 @@ const checkProcess = (value) => {
 }
 checkProcess(num)
 
-// #2
-// let string = 'whats_up';
-// const checkProcess = (value) => {
-//     console.log(value.length)
-// }
-// checkProcess(string)
+#2
+let string = 'whats_up';
+const checkProcess = (value) => {
+    console.log(value.length)
+}
+checkProcess(string)
 
-// #3
-// let string = 'whats_up';
-// const checkProcess = (value) => {
-//     return value.length - 1)
-//     return value.at(-1))
-// }
-// console.log(checkProcess(string))
+#3
+let string = 'whats_up';
+const checkProcess = (value) => {
+    return value.length - 1)
+    return value.at(-1))
+}
+console.log(checkProcess(string))
 
-// #4
-// let number = 25;
+#4
+let number = 25;
 
-// const checkProcess = (value) => {
-//     return value % 2 == 0;
-// }
-// console.log(checkProcess(number));
+const checkProcess = (value) => {
+    return value % 2 == 0;
+}
+console.log(checkProcess(number));
 
 #5
 
@@ -52,7 +52,6 @@ console.log(checkProcess(word1, word2));
 
 Level 1.2 task JavaScript
 #1
-
 let number = 25545;
 const checkProcess = (value) => {
     return value.toString().at(0);

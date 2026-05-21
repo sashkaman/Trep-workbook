b7d0a3d8da5d96665b8afb3c919cd874d26bf55b
Alexander <rzn.russia4912@gmail.com>
2026-05-21 22:33:07 +0300

    start of tasks 1.3 	modified:   Tasks -1.js

--- Tasks -1.js
+++ Tasks -1.js
@@ -1,47 +1,12 @@
-Level 1.2 task JavaScript
+Level 1.3 task JavaScript
+
 #1
-let number = 25545;
-const checkProcess = (value) => {
-    return value.toString().at(0);
-}
-console.log(checkProcess(number));
+let string = 'asdf;lkja'
 
-#2
-let number = 25545;
-const checkProcess = (value) => {
-    return value.toString().at(-1);
-}
-console.log(checkProcess(number));
-
-#3
-let number = 25545;
-const checkProcess = (num) => {
-    return +num.toString().at(0) + +num.toString().at(-1);
-}
-console.log(checkProcess(number));
-
-#4
-let number = 255;
-const checkProcess = (num) => {
-    return String(num).length
-}
-console.log(checkProcess(number));
-
-#5
-let number1 = 454324;
-let number2 = 4324;
-const checkProcess = (num1, num2) => {
-    let res = String(num1).at(0) === String(num2).at(0)
-    return res
+const checkString = (str) => {
+    if (str.length > 0) {
+        return str.at(-2)
+    }
 }
 
-console.log(checkProcess(number1, number2))
-
-// solution option #5
-const checkProcess = (num1, num2) => {
-    const s1 = String(Math.abs(num1));
-    const s2 = String(Math.abs(num2));
-
-    return s1[0] === s2[0];
-};
-
+console.log(checkString(string));

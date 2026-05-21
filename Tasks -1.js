d3636720626d660ea255f81a73b8cce48b1be2e3
Alexander <rzn.russia4912@gmail.com>
2026-05-21 22:31:28 +0300

    level 1.2 - 5 tasks 	modified:   Tasks -1.js

--- Tasks -1.js
+++ Tasks -1.js
@@ -1,55 +1,3 @@
-Level 1.1 task JavaScript
-
-#1
-
-let num = -24234;
-const checkProcess = (value) => {
-    return value > 0 ? value > 0 : value < 0
-}
-checkProcess(num)
-
-#2
-let string = 'whats_up';
-const checkProcess = (value) => {
-    console.log(value.length)
-}
-checkProcess(string)
-
-#3
-let string = 'whats_up';
-const checkProcess = (value) => {
-    return value.length - 1)
-    return value.at(-1))
-}
-console.log(checkProcess(string))
-
-#4
-let number = 25;
-
-const checkProcess = (value) => {
-    return value % 2 == 0;
-}
-console.log(checkProcess(number));
-
-#5
-
-let word1 = 'what_up man';
-let word2 = 'what is your name?';
-
-const checkProcess = (value1, va2lue2) => {
-    return value1.at(0) == value.at(0);
-}
-console.log(checkProcess(word1, word2));
-
-#6
-let word1 = 'what_up man';
-let word2 = 'what is your name?';
-
-const checkProcess = (value1, va2lue2) => {
-    return value1.at(0) == value.at(0);
-}
-console.log(checkProcess(word1, word2));
-
 Level 1.2 task JavaScript
 #1
 let number = 25545;
@@ -97,15 +45,3 @@ const checkProcess = (num1, num2) => {
     return s1[0] === s2[0];
 };
 
-Level 1.3 task JavaScript
-
-#1
-let string = 'asdf;lkja'
-
-const checkString = (str) => {
-    if (str.length > 0) {
-        return str.at(-2)
-    }
-}
-
-console.log(checkString(string));

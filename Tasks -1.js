198f82e9748906638fd35e85df22b6e514d8419e
Alexander <rzn.russia4912@gmail.com>
2026-05-24 14:57:27 +0300

    task 1.3 is done 	modified:   Tasks -1.js

--- Tasks -1.js
+++ Tasks -1.js
@@ -10,3 +10,18 @@ const checkString = (str) => {
 }
 
 console.log(checkString(string));
+
+#2
+
+let num1 = 51;
+let num2 = 25;
+
+const checkNumber = (value1, value2) => {
+    if (value1 % value2 === 0) {
+        return 0
+    } else {
+        return 'countless '
+    }
+}
+
+console.log(checkNumber(num1, num2));

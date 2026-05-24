5434066deef6f7720b0b1a08e36652ebeaa8e7b3
Alexander <rzn.russia4912@gmail.com>
2026-05-24 23:04:59 +0300

    added 2 tasks 	modified:   Tasks -1.js

---
+++ Tasks -1.js
@@ -0,0 +1,20 @@
+// Level 1.4
+
+// #1
+// const count = (value) => {
+//     for (let i = 1; i <= value; i++) {
+//         console.log(i);
+//     }
+// }
+
+// count(100);
+
+
+// #2
+// const countFromMinus100 = (value) => {
+//     for (let i = value; i <= 0; i++) {
+//         console.log(i);
+//     }
+// }
+
+// countFromMinus100(-100);

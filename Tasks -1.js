c82b8490e22833e2f7715c98c3427a7780215fb5
Alexander <rzn.russia4912@gmail.com>
2026-05-27 15:58:56 +0300

    Level 5 is done 	modified:   Tasks -1.js

--- Tasks -1.js
+++ Tasks -1.js
@@ -1,19 +1,78 @@
 // #1
-let item = 100;
+// let item = 100;
 
-const sumNumber = (value) => {
-    let sum = 0;
-    for (let i = 0; i <= value; i++) {
-        sum += i;
-    }
-    console.log(sum)
-}
+// const sumNumber = (value) => {
+//     let sum = 0;
+//     for (let i = 0; i <= value; i++) {
+//         sum += i;
+//     }
+//     console.log(sum)
+// }
 
-sumNumber(item)
+// sumNumber(item)
 
 // #1 >
-const item = 100;
+// const item = 100;
 
-const sumNumber = (value) => (value * (value + 1)) / 2;
+// const sumNumber = (value) => (value * (value + 1)) / 2;
 
-console.log(sumNumber(item));
+// console.log(sumNumber(item));
+
+// #2
+
+// let item = 100;
+
+// const sumNumber = (value) => {
+//     let sum = 0;
+//     for (let i = 0; i <= value; i++) {
+//         if (i % 2 === 0) {
+//             sum += i;
+//         }
+//     }
+//     console.log(sum)
+// }
+
+// sumNumber(item)
+
+// #3
+
+// let item = 100;
+
+// const getEventNumberSum = (value) => {
+//     let sumEvent = 0;
+//     for (let i = 0; i <= value; i++) {
+//         if (i % 2 !== 0) {
+//             sumEvent += i;
+//         }
+//     }
+//     return sumEvent
+// }
+
+// console.log(getEventNumberSum(item))
+
+// #4
+
+// let num1 = 234;
+// let num2 = 122;
+
+// const getRemainder = (value1, value2) => {
+//     return num1 % num2
+// }
+
+// console.log(getRemainder(num1, num2))
+
+// #5
+
+// let string = "Whatsup-HowAreYou"
+
+// const getItemsStringForEnd = (value) => {
+//     for (let i = string.length - 1; i >= 0; i--) {
+//         console.log(string[i])
+//     }
+// }
+
+// getItemsStringForEnd(string)
+
+
+
+
